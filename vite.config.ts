b624e848fafff575ee5264155e509440e7f25a6c
Akinori Nakata <akinori.nakata@bright-century.com>
2024-08-17 15:28:34 +0900

    Vite server 設定

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,20 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: true,
+    port: 4000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
     }
   }
 })
