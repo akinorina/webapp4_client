13e96327c3349a06992a17a63e3b6a7f0c49ceb5
Akinori Nakata <akinori.nakata@bright-century.com>
2024-10-26 16:33:01 +0900

    id class style 属性 不要のため削除

--- src/components/general/ButtonGeneral.vue
+++ src/components/general/ButtonGeneral.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
 export interface Props {
-  id?: string
-  className?: string
-  style?: string
   type?: any
 }
-const { id = '', className = '', style = '', type = 'button' } = defineProps<Props>()
+const { type = 'button' } = defineProps<Props>()
 </script>
 
 <template>
   <button
-    :id="id"
     :type="type"
     class="rounded-md bg-sky-500 px-3 py-1 text-slate-50 hover:bg-sky-700"
-    :class="className"
-    :style="style"
     @click="$emit('click')"
   >
     <slot></slot>
