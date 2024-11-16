c8161d92aef6a40c79fef0debb175d7f74feb177
Akinori Nakata <akinori.nakata@bright-century.com>
2024-11-16 15:21:35 +0900

    レイアウト修正

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import { RouterLink } from 'vue-router'
+import { useRouter, RouterLink } from 'vue-router'
 import { useImagePublicStore } from '@/stores/imagePublic'
+import ButtonGeneral from '@/components/general/ButtonGeneral.vue';
 
+const router = useRouter()
 const imagePublicStore = useImagePublicStore()
 
 const pathPrefix = import.meta.env.VITE_STORAGE_PATH_PREFIX
@@ -17,17 +19,48 @@ onMounted(() => {
     <div class="border p-3">
       <div class="text-xs">
         Web アプリケーション テンプレート ver.4 です。<br />
-        Database, SMTP, ObjectStorage などのミドルウェアとともに動作。<br />
+        Database, SMTP, ObjectStorage などのミドルウェアとともに動作します。<br />
         NestJSのRestAPI サーバー、および、Vue3 による クライアント により実現してます。
       </div>
     </div>
 
     <div class="m-1">
+
+      <div class="py-3">
+        <ButtonGeneral class="w-full my-0" @click="router.push({ name: 'admin' })">
+          <div class="">
+            <div class="text-xl font-bold">User Page</div>
+            <div class="text-xs">登録ユーザー用のページへ遷移。</div>
+          </div>
+        </ButtonGeneral>
+      </div>
+
+      <div class="py-3">
+        <ButtonGeneral class="w-full my-0" @click="router.push({ name: 'samples' })">
+          <div class="">
+            <div class="text-xl font-bold">Samples</div>
+            <div class="text-xs">各種機能のサンプルページ。</div>
+          </div>
+        </ButtonGeneral>
+      </div>
+
+      <div class="py-3">
+        <ButtonGeneral class="w-full my-0" @click="router.push({ name: 'blog' })">
+          <div class="">
+            <div class="text-xl font-bold">Blog</div>
+            <div class="text-xs">ブログ閲覧ページ</div>
+          </div>
+        </ButtonGeneral>
+      </div>
+
+
+
+      <!--
       <div class="grid grid-cols-2 gap-3">
         <div class="rounded border bg-slate-100 p-3">
           <router-link :to="{ name: 'admin' }" class="">
             <div class="">
-              <div class="font-bold">User Page</div>
+              <div class="text-xl font-bold">User Page</div>
               <div class="text-xs">登録ユーザー用のページへ遷移。</div>
             </div>
           </router-link>
@@ -36,7 +69,7 @@ onMounted(() => {
         <div class="rounded border bg-slate-100 p-3">
           <router-link :to="{ name: 'samples' }" class="">
             <div class="">
-              <div class="font-bold">Samples</div>
+              <div class="text-xl font-bold">Samples</div>
               <div class="text-xs">各種機能のサンプルページ。</div>
             </div>
           </router-link>
@@ -45,16 +78,17 @@ onMounted(() => {
         <div class="rounded border bg-slate-100 p-3">
           <router-link :to="{ name: 'blog' }" class="">
             <div class="">
-              <div class="font-bold">Blog</div>
+              <div class="text-xl font-bold">Blog</div>
               <div class="text-xs">ブログ閲覧ページ</div>
             </div>
           </router-link>
         </div>
       </div>
+      -->
     </div>
 
-    <div class="mt-16">
-      <div class="flex flex-wrap justify-center gap-8">
+    <div class="mt-8">
+      <div class="flex flex-wrap justify-center gap-4">
         <div
           class="flex size-40 items-center overflow-hidden border"
           v-for="(image, index) in imagePublicStore.images"
--- src/views/admin/ChangePasswordView.vue
+++ src/views/admin/ChangePasswordView.vue
@@ -52,27 +52,31 @@ const submitForm = async () => {
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <div class="border p-3">
-      <div class="mb-3 bg-green-300 p-3" v-if="resultSuccess">パスワードが変更されました。</div>
-      <div class="mb-3 bg-red-300 p-3" v-if="resultFailure">パスワード変更が失敗しました。</div>
+  <div class="container mx-auto p-2">
+    <div class="max-w-96 mx-auto border-0 border-red-600">
+      <div class="w-full mx-auto p-3 text-lg font-bold text-center">
+        パスワード変更
+      </div>
+
+      <div class="mb-3 bg-green-500 text-white font-bold p-3" v-if="resultSuccess">パスワードが変更されました。</div>
+      <div class="mb-3 bg-red-500 text-white font-bold p-3" v-if="resultFailure">パスワード変更が失敗しました。</div>
 
       <form class="" novalidate @submit.stop.prevent="submitForm">
-        <div class="flex flex-col items-center">
-          <div class="flex justify-start">
-            <label for="old_password" class="block w-48">これまでのパスワード</label>
-            <input-password class="w-80" id="old_password" v-model="passwordOldRaw" />
+        <div class="w-full flex flex-col items-center">
+          <div class="w-full m-3 flex">
+            <label  class="w-2/5 py-2" for="old_password">パスワード</label>
+            <input-password class="w-3/5 p-2 text-3xl" id="old_password" v-model="passwordOldRaw" />
           </div>
 
-          <div class="mt-3 flex justify-start">
-            <label for="new_password" class="block w-48">新しいパスワード</label>
-            <input-password class="w-80" id="new_password" v-model="passwordNewRaw" />
+          <div class="w-full m-3 flex">
+            <label class="w-2/5 py-2" for="new_password">新しいパスワード</label>
+            <input-password class="w-3/5 p-2 text-3xl" id="new_password" v-model="passwordNewRaw" />
           </div>
         </div>
 
         <div class="mt-5 text-center">
-          <ButtonGeneral type="button" class="me-2" @click="toAdmin">戻る</ButtonGeneral>
-          <ButtonGeneral type="submit" class="">更新</ButtonGeneral>
+          <ButtonGeneral type="button" class="me-2 bg-slate-400 hover:bg-slate-500" @click="toAdmin">戻る</ButtonGeneral>
+          <ButtonGeneral type="submit" class="me-0">更新</ButtonGeneral>
         </div>
       </form>
     </div>
